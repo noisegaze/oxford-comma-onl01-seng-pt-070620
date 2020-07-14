7ec9958bf111f48c741f2eace9abead37bc8ea32
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-14 16:51:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.length == 1
     array.join(",")
   elsif array.length == 2
-    array.insert(-2, " and ").join(", ")
+    array.join(", ").insert(-2, " and ")
   elsif array.length >= 3
-    array.insert(-2, " and ").join(", ")
+    array.join(", ").insert(-2, " and ")
   else
     array.join
   end   
